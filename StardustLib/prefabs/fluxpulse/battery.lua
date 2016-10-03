9b22dcc448e9576d6f4cf2a0e0afa1124fe9a827
zetaPRIME <qmanxt@gmail.com>
2016-10-03 07:05:17 -0400

    the first prefab: fluxpulse battery

---
+++ StardustLib/prefabs/fluxpulse/battery.lua
@@ -0,0 +1,39 @@
+require("/lib/stardust/power.lua") -- hopefully this won't derp things up??
+
+__prefab = {}
+local battery = __prefab -- I don't know
+
+function battery:new(capacity, ioRate)
+  self.capacity = capacity or 10000
+  ioRate = ioRate or capacity
+  
+  local iotype = type(ioRate)
+  if iotype == "table" then self.ioRate = ioRate
+  elseif iotype == "number" then self.ioRate = { input = ioRate, output = ioRate } end
+  
+  self.state = { energy = 0 }
+end
+
+function battery:hookUp(privateProvider, privateReceptor)
+  if not privateProvider then shared.energyProvider = self end
+  if not privateReceptor then shared.energyReceptor = self end
+  return self
+end
+
+function battery:autoSave(key)
+  key = key or "batteryState"
+  if storage[key] then self.state = storage[key] else storage[key] = self.state end
+  return self
+end
+
+function battery:extract(socket, amount, testOnly)
+  local amt = math.min(math.min(amount, self.ioRate.output), self.state.energy)
+  if not testOnly then self.state.energy = self.state.energy - amt end
+  return amt
+end
+
+function battery:receive(socket, amount, testOnly)
+  local amt = math.min(math.min(amount, self.ioRate.input), self.capacity - self.state.energy)
+  if not testOnly then self.state.energy = self.state.energy + amt end
+  return amt
+end

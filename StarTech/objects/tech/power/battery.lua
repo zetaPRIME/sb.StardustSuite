ad8533867f1a65c47e45df70fe627377c387a5e7
zetaPRIME <qmanxt@gmail.com>
2016-10-05 05:00:48 -0400

    moar battery stuffs

--- StarTech/objects/tech/power/battery.lua
+++ StarTech/objects/tech/power/battery.lua
@@ -8,7 +8,7 @@ function init()
   battery = prefabs.power.battery(cfg.capacity, cfg.ioRate):hookUp():autoSave()
   
   --message.setHandler("wrenchInteract", onWrench)
-  dDesc = root.itemConfig({ name = object.name(), count = 1 }).config.description
+  dDesc = config.getParameter("baseDescription")--root.itemConfig({ name = object.name(), count = 1 }).config.description
   tDesc = 573000000
 end
 
@@ -24,7 +24,7 @@ function update()
   if tDesc >= 10 then
     tDesc = 0
     if battery.state.energy ~= lastDescEnergy then
-      object.setConfigParameter("description", getDescription)
+      object.setConfigParameter("description", getDescription())
       lastDescEnergy = battery.state.energy
     end
   end
@@ -57,7 +57,52 @@ function die()
   if battery.state.energy >= 1 then
     itm.parameters.storedEnergy = battery.state.energy
     itm.parameters.description = getDescription()
+    
+    local batLevel = battery.state.energy / battery.capacity
+    itm.parameters.inventoryIcon = {
+      { image = "battery.frame.png" },
+      {
+        image = table.concat({
+          "battery.meter.png?addmask=/objects/tech/power/battery.meter.png", ";0;",
+          10 - math.floor(batLevel * 10),
+          "?multiply=", colorToString(hslToRgb(math.max(0, batLevel*1.25 - 0.25) * 1/3, 1, 0.5, 1))
+        }), 
+        fullbright = true
+      }
+    }
+    --itm.parameters.largeImage = itm.parameters.inventoryIcon -- maybe?
   end
   world.spawnItem(itm, entity.position())
   object.smash(true)
+end
+
+-- color stuffs
+function hslToRgb(h, s, l, a)
+  local r, g, b
+
+  if s == 0 then
+    r, g, b = l, l, l -- achromatic
+  else
+    function hue2rgb(p, q, t)
+      if t < 0   then t = t + 1 end
+      if t > 1   then t = t - 1 end
+      if t < 1/6 then return p + (q - p) * 6 * t end
+      if t < 1/2 then return q end
+      if t < 2/3 then return p + (q - p) * (2/3 - t) * 6 end
+      return p
+    end
+
+    local q
+    if l < 0.5 then q = l * (1 + s) else q = l + s - l * s end
+    local p = 2 * l - q
+
+    r = hue2rgb(p, q, h + 1/3)
+    g = hue2rgb(p, q, h)
+    b = hue2rgb(p, q, h - 1/3)
+  end
+
+  return {math.ceil(r * 255), math.ceil(g * 255), math.ceil(b * 255), math.ceil(a * 255)}
+end
+function colorToString(color)
+  return string.format("%08x", color[1] * 16777216 + color[2] * 65536 + color[3] * 256 + color[4])
 end

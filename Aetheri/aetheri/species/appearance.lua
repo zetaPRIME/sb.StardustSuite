bd15dfcacdf0204731b3e6e46e0203f89cbf746d
zetaPRIME <qmanxt@gmail.com>
2019-05-08 05:23:26 -0400

    split appearance into its own module (the first of many)

---
+++ Aetheri/aetheri/species/appearance.lua
@@ -0,0 +1,62 @@
+-- handles all appearance and animation apart from the HUD
+
+require "/lib/stardust/color.lua"
+
+appearance = {
+  baseDirectives = "",
+}
+
+local bodyReplacePalette = {
+  "dafafafa", "caeaeafa", "badadafa", "aacacafa"
+}
+
+local function generatePalette(tbl)
+  local hue = tbl[1]
+  local sat = tbl[2]
+  local lumBright = tbl[3]
+  local lumDark = tbl[4]
+  return {
+    color.toHex(color.fromHsl{ hue, sat, lumBright }),
+    color.toHex(color.fromHsl{ hue, sat, util.lerp(1/3, lumBright, lumDark) }),
+    color.toHex(color.fromHsl{ hue, sat, util.lerp(2/3, lumBright, lumDark) }),
+    color.toHex(color.fromHsl{ hue, sat, lumDark })
+  }
+end
+
+local directives = ""
+local updateGlow
+function appearance.updateColors()
+  appearance.settings = status.statusProperty("aetheri:appearance", { })
+  local a = appearance.settings
+  a.coreHsl = a.coreHsl or { 0.77, 1, 1.0, 0.64 }
+  a.palette = generatePalette(a.coreHsl)
+  a.glowColor = color.fromHsl {
+    a.coreHsl[1],
+    a.coreHsl[2],
+    (a.coreHsl[3] + a.coreHsl[4]) / 2
+  }
+  
+  status.setStatusProperty("aetheri:appearance", a)
+  
+  local d = {
+    "?replace;663b14fe=00000000;8d581cfe=00000000;c88b28fe=00000000;e7c474fe=00000000;404040fe=00000000;808080fe=00000000;6d0103fe=00000000;02da37fe=00000000;5786fffe=00000000",
+    color.replaceDirective(bodyReplacePalette, a.palette, true),
+  }
+  appearance.baseDirectives = table.concat(d)
+  tech.setParentDirectives(appearance.baseDirectives)
+  
+  playerext.setGlowColor(color.lightColor(a.glowColor, 0.64))
+  world.sendEntityMessage(entity.id(), "aetheri:paletteChanged")
+  updateGlow = true
+end
+
+function appearance.update(p)
+  if updateGlow then
+    updateGlow = false
+    local a = appearance.settings
+    playerext.setGlowColor(color.lightColor(a.glowColor, 0.64))
+  end
+end
+
+-- register these here since this is executed during techstub init
+message.setHandler("aetheri:refreshAppearance", appearance.updateColors)
--- Aetheri/aetheri/species/main.lua
+++ Aetheri/aetheri/species/main.lua
@@ -1,61 +1,17 @@
--- Aetheri tech override - this one's gonna get *big*
+-- Aetheri tech override - this one's gonna get *big* (so it's in modules for better navigation!)
+
+-- common libraries
 require "/lib/stardust/playerext.lua"
-require "/lib/stardust/color.lua"
 
-local bodyReplacePalette = {
-  "dafafafa", "caeaeafa", "badadafa", "aacacafa"
-}
-
-local function generatePalette(tbl)
-  local hue = tbl[1]
-  local sat = tbl[2]
-  local lumBright = tbl[3]
-  local lumDark = tbl[4]
-  return {
-    color.toHex(color.fromHsl{ hue, sat, lumBright }),
-    color.toHex(color.fromHsl{ hue, sat, util.lerp(1/3, lumBright, lumDark) }),
-    color.toHex(color.fromHsl{ hue, sat, util.lerp(2/3, lumBright, lumDark) }),
-    color.toHex(color.fromHsl{ hue, sat, lumDark })
-  }
-end
-
-local directives = ""
-local updateGlow
-local function updateColors()
-  local appearance = status.statusProperty("aetheri:appearance", { })
-  appearance.coreHsl = appearance.coreHsl or { 0.77, 1, 1.0, 0.64 }
-  appearance.palette = generatePalette(appearance.coreHsl)
-  appearance.glowColor = color.fromHsl {
-    appearance.coreHsl[1],
-    appearance.coreHsl[2],
-    (appearance.coreHsl[3] + appearance.coreHsl[4]) / 2
-  }
-  
-  status.setStatusProperty("aetheri:appearance", appearance)
-  
-  local d = {
-    "?replace;663b14fe=00000000;8d581cfe=00000000;c88b28fe=00000000;e7c474fe=00000000;404040fe=00000000;808080fe=00000000;6d0103fe=00000000;02da37fe=00000000;5786fffe=00000000",
-    color.replaceDirective(bodyReplacePalette, appearance.palette, true),
-  }
-  directives = table.concat(d)
-  tech.setParentDirectives(directives)
-  
-  playerext.setGlowColor(color.lightColor(appearance.glowColor, 0.64))
-  world.sendEntityMessage(entity.id(), "aetheri:paletteChanged")
-  updateGlow = true
-end
+-- and modules
+require "/aetheri/species/appearance.lua"
 
 function init()
-  updateColors()
-  message.setHandler("aetheri:refreshAppearance", updateColors)
+  appearance.updateColors()
 end
 
 function update(p)
-  if updateGlow then
-    updateGlow = false
-    local appearance = status.statusProperty("aetheri:appearance", { })
-    playerext.setGlowColor(color.lightColor(appearance.glowColor, 0.64))
-  end
+  appearance.update(p)
 end
 
 

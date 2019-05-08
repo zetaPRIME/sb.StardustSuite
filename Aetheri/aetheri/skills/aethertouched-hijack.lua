5c36860c44999f994f357376aeae4de6bd4bf227
zetaPRIME <qmanxt@gmail.com>
2019-05-08 01:57:47 -0400

    make aethertouched work by contribution and not proximity

--- Aetheri/aetheri/skills/aethertouched-hijack.lua
+++ Aetheri/aetheri/skills/aethertouched-hijack.lua
@@ -4,11 +4,30 @@ do
   local npc = monster or npc
   --npc.say("lol internet")
   
+  local contributors = { }
+  
+  function _aethertouched_addcontributor(id)
+    if not id then return nil end
+    contributors[id] = true
+  end
+  
+  -- add anyone who does damage after an aether skill to the contributor list
+  local _damage = damage or function() end
+  function damage(args)
+    _aethertouched_addcontributor(args.sourceId)
+    _damage(args)
+  end
+  
+  -- grant experience to all (Aetheri) contributors when killed
   local _die = die or function() end
   function die(...)
-    for _, p in pairs(world.playerQuery(entity.position(), 25*2)) do
+    -- first calculate granted xp
+    local experience = math.floor(0.5 + world.entityHealth(entity.id())[2]) * 10)
+    
+    -- then loop through and send
+    for p in pairs(contributors) do
       if world.entitySpecies(p) == "aetheri" then
-        world.sendEntityMessage(p, "playerext:message", string.format("A(n) %s died; %f experience gained", world.entityTypeName(entity.id()), world.entityHealth(entity.id())[2]))
+        world.sendEntityMessage(p, "playerext:message", string.format("Killed %s; %d experience gained", world.entityTypeName(entity.id()), experience))
       end
     end
     _die(...)
--- Aetheri/aetheri/skills/aethertouched.lua
+++ Aetheri/aetheri/skills/aethertouched.lua
@@ -2,4 +2,5 @@
 
 function init()
   world.callScriptedEntity(entity.id(), "require", "/aetheri/skills/aethertouched-hijack.lua")
+  world.callScriptedEntity(entity.id(), "_aethertouched_addcontributor", effect.sourceEntity()) -- make the applier still count if oneshot
 end

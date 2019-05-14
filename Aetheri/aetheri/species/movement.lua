16086f7fc8a27ecb81b2ca98ae595848dc8776fb
zetaPRIME <qmanxt@gmail.com>
2019-05-13 22:15:28 -0400

    Aetheri: air jump

--- Aetheri/aetheri/species/movement.lua
+++ Aetheri/aetheri/species/movement.lua
@@ -28,7 +28,7 @@ end
 
 movement.states.ground = { }
 function movement.states.ground:init()
-  --
+  self.airJumps = 0
 end
 
 function movement.states.ground:uninit()
@@ -38,10 +38,26 @@ end
 function movement.states.ground:update(dt)
   if mcontroller.onGround() then
     self.sprinting = input.key.sprint and input.dir[1] ~= 0
+    self.airJumps = stats.stat.airJumps
   end
   if self.sprinting then
     mcontroller.controlMove(input.dir[1], true) -- set running
     -- sprint speed and a bit of a jump boost
     mcontroller.controlModifiers({ speedModifier = 1.5, airJumpModifier = 1.35 })
+  end
+  
+  -- air jump!
+  if not mcontroller.onGround()
+  and not mcontroller.liquidMovement()
+  and not mcontroller.jumping()
+  --and mcontroller.yVelocity() < 0
+  and input.keyDown.jump and self.airJumps >= 1 then
+    self.airJumps = self.airJumps - 1
+    mcontroller.controlJump(true)
+    mcontroller.setYVelocity(math.max(0, mcontroller.yVelocity()))
+    mcontroller.controlParameters({ airForce = 1750.0 }) -- allow easier direction control during jump
+    self.sprinting = self.sprinting or (input.key.sprint and input.dir[1] ~= 0) -- allow starting a sprint from an air jump
+    sound.play("/sfx/tech/tech_doublejump.ogg")
+    -- TODO: particle/animation stuff
   end
 end

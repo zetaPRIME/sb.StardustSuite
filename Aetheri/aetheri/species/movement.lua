3449a3a1762400c499948844d83b9a144aeb7aa6
zetaPRIME <qmanxt@gmail.com>
2019-05-16 10:09:44 -0400

    Aetheri: fleshed out rail grinding

--- Aetheri/aetheri/species/movement.lua
+++ Aetheri/aetheri/species/movement.lua
@@ -15,8 +15,9 @@ local function railCast(pos, dist)
       local relX = pos[1] - sp[1]
       local y = math.ceil(p[2]) -- top of region
       local slope = 0
-      if railCheck(vec2.add(p, {0, 1})) then
-        -- rail directly above; flat slope
+      if railCheck(vec2.add(p, {0, 1})) then -- rail directly above
+        -- don't land on the middle of a vertical rail
+        if not railCheck(vec2.add(p, {-1, 0})) and not railCheck(vec2.add(p, {1, 0})) then return false end
       elseif relX >= 0 then -- on the right
         if railCheck(vec2.add(p, {1, 1})) and not railCheck(vec2.add(p, {1, 2})) then slope = -1
         elseif railCheck(vec2.add(p, {1, 0})) then slope = 0
@@ -66,10 +67,11 @@ end
 -- for now, states are just part of the movement module; this may change... eventually
 
 movement.states.ground = { }
-function movement.states.ground:init()
-  self.airJumps = 0
+function movement.states.ground:init(_, _, giveAirJumps, sprinting)
+  self.airJumps = giveAirJumps and stats.stat.airJumps or 0
   self.airJumpTimer = 0
   self.groundTimer = 0
+  self.sprinting = sprinting
 end
 
 function movement.states.ground:uninit()
@@ -82,13 +84,16 @@ function movement.states.ground:update(dt)
   mcontroller.clearControls()
   mcontroller.controlModifiers { speedModifier = stats.stat.moveSpeed }
   
-  local rc = railCast(vec2.add(vec2.add(mcontroller.position(), vec2.mul(mcontroller.velocity(), dt)), {0, -2.51}), 1)
-  if rc and input.key.sprint then
-    tech.setParentState("Duck")
+  local rc = railCast(vec2.add(mcontroller.position(), {0, -2.51}), 0)
+  rc = rc or railCast(vec2.add(mcontroller.position(), {mcontroller.xVelocity() * dt, -2.51}), 0)
+  if rc and input.key.sprint and input.key.down and mcontroller.yVelocity() <= 0 then
+    return movement.enterState("rail")
+    --[[tech.setParentState("Duck")
     mcontroller.setYVelocity(0)
     mcontroller.setYPosition(rc.point[2] + 2.5)
-    mcontroller.addMomentum({rc.slope * dt * 250, 0})
-    mcontroller.setRotation(math.pi * rc.slope * -0.25)
+    mcontroller.addMomentum({rc.slope * dt * 75, 0})
+    --mcontroller.setRotation(math.pi * rc.slope * -0.25)
+    ]]
   end
   
   if mcontroller.canJump() then self.groundTimer = 0.2 end
@@ -155,3 +160,109 @@ function movement.states.ground:update(dt)
   end
   
 end
+
+do
+  local function offsetForRot(rot)
+    return (math.cos(rot) - 1) * -4, (math.sin(rot)) * -1.5
+  end
+  
+  local function towards(cur, target, max)
+    max = math.abs(max)
+    if max == 0 then return cur end
+    if target == cur then return target end
+    local diff = target - cur
+    local sign = diff / math.abs(diff)
+    return cur + math.min(math.abs(diff), max) * sign
+  end
+  
+  local function rotateTowards(self, rot, dt)
+    local curRot = mcontroller.rotation()
+    local limit = util.clamp(math.pi * 2 * dt, 0.025, math.abs(curRot - rot) / 2)
+    local curYOffset = self.yOffset
+    local curXOffset = self.xOffset
+    local newRot = towards(curRot, rot, limit)
+    self.yOffset, self.xOffset = offsetForRot(newRot)
+    mcontroller.setRotation(newRot)
+    mcontroller.setYPosition(mcontroller.yPosition() + self.yOffset - curYOffset)
+    mcontroller.setXPosition(mcontroller.xPosition() + self.xOffset - curXOffset)
+    --
+  end
+  
+  movement.states.rail = { }
+  function movement.states.rail:init()
+    self.xOffset = 0
+    self.yOffset = 0
+    self.lastSlope = 0
+    sound.play("/aetheri/sfx/railGrindHit.ogg", 0.64, 1.1)
+    self.sfx = sound.newLoop("/aetheri/sfx/railGrindLoop.ogg")
+    
+    -- stuff
+    local rc = railCast(vec2.add(mcontroller.position(), {0, -2.51 - self.yOffset}), 2)
+    if rc then
+      mcontroller.setVelocity(vec2.rotate(mcontroller.velocity(), math.pi * rc.slope * 0.25))
+      rotateTowards(self, math.pi * rc.slope * -0.25, 10000)
+    end
+  end
+  
+  function movement.states.rail:uninit()
+    self.sfx:discard()
+    --mcontroller.setYVelocity(mcontroller.yVelocity() + mcontroller.xVelocity() * self.lastSlope * -1)
+    local vel = mcontroller.velocity()
+    vel[2] = math.max(0, vel[2])
+    vel = vec2.mul(vec2.rotate(vel, mcontroller.rotation() --[[math.pi * self.lastSlope * -0.25]]), {1, 1})
+    mcontroller.setVelocity(vel)
+    if vel[2] > 0 and not self.forceJump then mcontroller.controlJump(true) end
+  end
+  
+  function movement.states.rail:update(dt)
+    mcontroller.clearControls()
+    
+    if not input.key.sprint then return movement.enterState("ground", true, true) end
+    if input.keyDown.jump then
+      mcontroller.addMomentum({0, 75})
+      self.forceJump = true
+      return movement.enterState("ground", true, true)
+    end
+    
+    local rc = railCast(vec2.add(mcontroller.position(), {0, -2.51 - self.yOffset}), 2)
+    if not rc then rc = railCast(vec2.add(mcontroller.position(), {0, -1.51 - self.yOffset}), 3) end
+    if rc then
+      tech.setParentState("Duck")
+      mcontroller.setYVelocity(0)
+      mcontroller.setYPosition(rc.point[2] + 2.5 + self.yOffset)
+      mcontroller.addMomentum({rc.slope * dt * 75, 0})
+      mcontroller.addMomentum({input.dir[1] * dt * 25, 0})
+      local rslope = rc.slope
+      local rc2 = railCast(vec2.add(mcontroller.position(), {-self.xOffset * 2, -2.0}), 3)
+      if rc2 and rc2.slope == 0 then rslope = 0 end
+      rotateTowards(self, math.pi * rslope * -0.25, dt)
+      self.lastSlope = rc.slope
+    else -- no rail!
+      return movement.enterState("ground", true, true)
+    end
+    
+    local speed = math.abs(mcontroller.xVelocity())
+    speed = math.min(1, speed / 25)
+    self.sfx:setVolume(speed * 0.5)
+    self.sfx:setPitch(1)
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- EOF --

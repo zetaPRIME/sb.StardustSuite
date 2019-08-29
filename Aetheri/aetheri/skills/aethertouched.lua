1ac48581198535bfd781731926a83907dfa1b45c
zetaPRIME <qmanxt@gmail.com>
2019-08-29 02:17:38 -0400

    Aetheri: `damage()` is called *after* statuses applied

--- Aetheri/aetheri/skills/aethertouched.lua
+++ Aetheri/aetheri/skills/aethertouched.lua
@@ -2,5 +2,4 @@
 
 function init()
   world.callScriptedEntity(entity.id(), "require", "/aetheri/skills/aethertouched-hijack.lua")
-  world.callScriptedEntity(entity.id(), "_aethertouched_addcontributor", effect.sourceEntity()) -- make the applier still count if oneshot
 end

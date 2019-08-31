045ac201a85d7460cde17254b386e9e5d6b42455
zetaPRIME <qmanxt@gmail.com>
2019-08-31 04:50:59 -0400

    beginning of configurator rework

---
+++ StarTech/startech/quickbar/configurator.lua
@@ -0,0 +1,28 @@
+require "/lib/stardust/itemutil.lua"
+require "/lib/stardust/playerext.lua"
+
+local tierReqs = {
+  { name = "ironbar", count = 5 },
+  { name = "tungstenbar", count = 5 },
+  { name = "titaniumbar", count = 15 },
+  { name = "durasteelbar", count = 20 },
+  { name = "refinedviolium", count = 20 },
+  { name = "solariumstar", count = 20 },
+}
+
+do
+  local itm = player.swapSlotItem() or { }
+  local uiDef = itemutil.property(itm, "/startech:configuratorUi")
+  if not uiDef then
+    pane.playSound("/sfx/interface/clickon_error.ogg")
+    playerext.message("Must be holding configurable item.")
+    return nil
+  end
+  if type(uiDef) == "string" then
+    player.interact("ScriptPane", uiDef)
+  elseif type(uiDef) == "table" then
+    player.interact(table.unpack(uiDef))
+  end
+  
+  
+end

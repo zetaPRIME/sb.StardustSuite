4fc386bb741ee9a2bca739d412604ab2e21f58c8
zetaPRIME <qmanxt@gmail.com>
2019-09-24 04:05:37 -0400

    some catalysts

---
+++ StarTech/startech/items/catalysts/catalyst.lua
@@ -0,0 +1,16 @@
+require "/scripts/augments/item.lua"
+
+function apply(input)
+  local data = config.getParameter("catalystData") or { }
+  local output = Item.new(input)
+  if not output:instanceValue("acceptsUpgradeCatalyst") then return nil end -- only on compatible items (pulse weapons etc.)
+  
+  if data.level then
+    if (output:instanceValue("level") or 1) > data.level then return nil end -- no non-upgrades
+    output:setInstanceValue("level", data.level)
+  end
+  
+  output:setInstanceValue("catalystData", data)
+  
+  return output:descriptor(), 1
+end

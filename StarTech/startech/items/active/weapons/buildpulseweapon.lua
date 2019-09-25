cf620d5132c5b749196f573f71b3b89f7a9e7a26
zetaPRIME <qmanxt@gmail.com>
2019-09-25 00:35:45 -0400

    catalyst recipes!

--- StarTech/startech/items/active/weapons/buildpulseweapon.lua
+++ StarTech/startech/items/active/weapons/buildpulseweapon.lua
@@ -6,8 +6,15 @@ function build(directory, config, parameters, level, seed)
   level = parameters.level or config.level or level or 1
   parameters.level = level
   
+  local lvLabel = string.format("^gray;Level: ^white;%i^reset;", math.floor(0.5 + level))
+  if parameters.catalystData and parameters.catalystData.name then
+    lvLabel = string.format("%s ^lightgray;(%s)^reset;", lvLabel, parameters.catalystData.name)
+  else
+    lvLabel = string.format("%s ^darkgray;(no catalyst)^reset;", lvLabel)
+  end
+  
   parameters.tooltipFields = {
-    bottomLabel = string.format("^gray;Level: ^white;%i", level)
+    bottomLabel = lvLabel
   }
   
   return config, parameters

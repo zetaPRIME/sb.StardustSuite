79aadc1ca44dc559349cba94ffff7eb7d3507442
zetaPRIME <qmanxt@gmail.com>
2019-09-27 18:36:04 -0400

    fp meter improvements

--- StarTech/startech/items/power/armor/nanofield/hud.lua
+++ StarTech/startech/items/power/armor/nanofield/hud.lua
@@ -13,6 +13,15 @@ end
 
 local showTime = 0
 
+local charWidth = {
+  default = 4,
+  A = 6,
+  ["%"] = 7,
+}
+local charColor = {
+  A = "?multiply=9771e4"
+}
+
 function hud.update(p)
   --
   if showTime > 0 then
@@ -20,22 +29,32 @@ function hud.update(p)
     showTime = showTime - p.dt
     
     local cur, max = table.unpack(playerext.readEquipEnergy())
+    if not max or max <= 0 then showTime = 0 return nil end
     local percent = math.ceil(100 * cur / max)
+    
+    local defColor = percent > 15 and "" or percent > 5 and "?multiply=ff7f7f" or "?multiply=ff0000"
     
     local numWidth = 5
     local drw = { }
-    local str = string.format("%d%%", percent)
-    --local str = string.format("%d", safeFloor(0.5 + status.resourcePercentage("aetheri:mana") * 100))
-    local off = ((str:len() - 1) * numWidth - 4) * -0.5
-    local mult = color.alphaDirective(alpha)
-    local y = -28 * px
+    local str = string.format("A%d%%", percent)
+    local width = str:len() - 1
     for i = 1, str:len() do
+      width = width + (charWidth[str:sub(i, i)] or charWidth.default)
+    end
+    
+    local off = -width/2--((str:len() - 1) * numWidth - 4) * -0.5
+    local mult = color.alphaDirective(alpha)
+    local x, y = 0, -28 * px
+    for i = 1, str:len() do
+      local ch = str:sub(i, i)
+      local color = charColor[ch] or defColor
       table.insert(drw, {
-        position = { (off + (i - 1) * numWidth) * px, y },
-        image = string.format("/startech/items/power/armor/nanofield/hud.png:num%s%s", str:sub(i, i), mult),
+        position = { (4 + off + x) * px, y },
+        image = string.format("/startech/items/power/armor/nanofield/hud.png:num%s%s%s", ch, color, mult),
         fullbright = true,
         renderLayer = hudLayer
       })
+      x = x + (charWidth[ch] or charWidth.default) + 1
     end
     playerext.queueDrawable(table.unpack(drw))
   end

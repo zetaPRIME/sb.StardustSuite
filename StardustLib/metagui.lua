be1495a7cc0abae895999162f1144997e144de95
zetaPRIME <qmanxt@gmail.com>
2020-03-05 22:08:06 -0500

    some startup for metaGUI

---
+++ StardustLib/metagui.lua
@@ -0,0 +1,22 @@
+-- metaGUI stub
+-- INCOMPLETE, DO NOT COPY YET
+-- this file should never need to change and can therefore be included UNMODIFIED in other mods
+
+function init() pane.dismiss()
+  _mgcfg = root.assetJson("/panes.config").metaGUI
+  if not _mgcfg then -- attempt to use fallback if metaGUI isn't present
+    local fb = config.getParameter("fallback")
+    if not fb then return player.confirm {
+        paneLayout = "/interface/windowconfig/popup.config:paneLayout",
+        icon = "/interface/errorpopup/erroricon.png",
+        title = "metaGUI error",
+        message = "Could not detect metaGUI/StardustLib, and interaction does not specify a fallback."
+      } end
+    if type(fb) ~= "table" or not fb[1] then fb = { "ScriptPane", fb } end
+    player.interact(fb[1], fb[2], pane.sourceEntity())
+  else
+    require "/sys/metagui/build.lua"
+  end
+  
+  
+end
---
+++ StardustLib/sys/metagui/build.lua
@@ -0,0 +1,55 @@
+-- pane builder
+
+if not _mgcfg then _mgcfg = root.assetJson("/panes.config").metaGUI end -- make sure we have this
+local registry = root.assetJson("/metagui/registry.json")
+
+-- determine UI json
+local uicfg = config.getParameter("config")
+if type(uicfg) == "string" then
+  while uicfg and uicfg:sub(1, 1) ~= "/" do -- not path; resolve from registry
+    local modname = uicfg:match('^(.-):')
+    local uiname = uicfg:match(':(.+)$')
+    uicfg = (registry.panes[modname] or { })[uiname]
+  end
+  -- still a string after resolving (if necessary)?
+  if type(uicfg) == "string" then uicfg = root.assetJson(uicfg) end
+end
+if type(uicfg) ~= "table" then
+  return nil -- error?
+end
+
+-- determine theme and accent color in use
+local defaultTheme = _mgcfg.defaultTheme
+if not registry.themes[defaultTheme] then for k in pairs(registry.themes) do defaultTheme = k break end end
+
+playercfg = status.statusProperty("metaGUI") or { }
+local theme = playercfg.theme or defaultTheme
+if not registry.themes[theme] then theme = defaultTheme end
+
+local themedata = root.assetJson(registry.themes[theme] .. "theme.json")
+
+local defaultAccentColor = playercfg.accentColor or themedata.defaultAccentColor
+
+-- apply config to ui data
+uicfg.theme = theme
+uicfg.themePath = registry.themes[theme]
+uicfg.accentColor = uicfg.accentColor or defaultAccentColor
+
+-- actually construct the base
+uicfg.style = uicfg.style or "window" -- default window style
+
+local size = uicfg.size
+local borderMargins = themedata.metrics.borderMargins[uicfg.style]
+size[1] = size[1] + borderMargins[1] + borderMargins[3]
+size[2] = size[2] + borderMargins[2] + borderMargins[4]
+
+player.interact("ScriptPane", {
+  gui = {
+    background = {
+      type = "background",
+      fileFooter = "/assetmissing.png?crop=0;0;1;1?multiply=0000?scalenearest=" .. size[1] .. ";" .. size[2]
+    }
+  },
+  scripts = { "/sys/metagui/core.lua" },
+  ___ = uicfg
+}, pane.sourceEntity())

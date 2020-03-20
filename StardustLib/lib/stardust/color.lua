a26bd02a628268c7a984df3d23f60f9db68c554f
zetaPRIME <qmanxt@gmail.com>
2020-03-20 17:35:22 -0400

    fix rgb to hsl

--- StardustLib/lib/stardust/color.lua
+++ StardustLib/lib/stardust/color.lua
@@ -100,13 +100,13 @@ do
   
   local function rgbToHsl(r, g, b)
     local max, min = math.max(r, g, b), math.min(r, g, b)
-    local b = max + min
-    local h = b / 2
+    local t = max + min
+    local h = t / 2
     if max == min then return 0, 0, h end
     local s, l = h, h
     local d = max - min
-    s = l > .5 and d / (2 - b) or d / b
-    if max == r then h = (g - b) / d + (g < b and 6 or 0)
+    s = l > .5 and d / (2 - t) or d / t
+    if max == r then h = (g - b) / d % 6
     elseif max == g then h = (b - r) / d + 2
     elseif max == b then h = (r - g) / d + 4
     end

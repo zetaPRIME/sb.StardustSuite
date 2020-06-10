51619ef4df70acd35a51b15644f27f6df3327923
zetaPRIME <qmanxt@gmail.com>
2020-06-10 06:32:45 -0400

    more misc work

--- StarTech/startech/ui/configurator/tree.lua
+++ StarTech/startech/ui/configurator/tree.lua
@@ -2,19 +2,21 @@
 require "/sys/stardust/skilltree/skilltree.lua"
 
 local function loadItem()
-  local nf = player.equippedItem("chest")
-  if (nf or { }).name ~= "startech:nanofield" then return pane.dismiss() end
-  return nf
+  local itm = player.swapSlotItem()
+  if not itm or not itemutil.property(itm, "stardustlib:skillTree") then return pane.dismiss() end
+  cfgSlot:setItem(itm)
+  player.setSwapSlotItem()
+  local title = itemutil.property(itm, "shortdescription") or "(unknown item)"
+  metagui.startEvent(function()
+    metagui.setTitle(title)
+    coroutine.yield()
+    metagui.setTitle(title)
+  end)
+  return itm
 end
 
 local function saveItem(itm)
-  do -- all we need to modify from the current item is the skill tree data
-    local c = player.equippedItem("chest")
-    c.parameters["stardustlib:skillData"] = itm.parameters["stardustlib:skillData"]
-    itm = c
-  end
-  
-  -- apply stats
+  --[[ apply stats
   local skillData = itm.parameters["stardustlib:skillData"]
   local calc = skilltree.calculateFinalStat
   local stats = { }
@@ -28,9 +30,9 @@ local function saveItem(itm)
     { stat = "stardustlib:leech", amount = calc(skillData.stats.leech) },
   })
   util.appendLists(stats, skillData.effects) -- carry over node status effects
-  itm.parameters.statusEffects = stats
+  itm.parameters.statusEffects = stats -- ]]
   
-  player.setEquippedItem("chest", itm)
+  cfgSlot:setItem(itm)
 end
 
 function init()
@@ -53,11 +55,15 @@ function toggleStats:onClick()
 end
 
 function update()
-  -- canary
-  local function nope() skilltree.playSound "reset" pane.dismiss() end
-  local itm = player.equippedItem("chest")
-  if not itm or itm.name ~= "startech:nanofield" then return nope() end
-  local sd = itm.parameters["stardustlib:skillData"]
-  if not sd then return nope() end
-  if sd.uuid ~= skilltree.uuid then return nope() end
+  -- ...
+end
+
+function uninit()
+  local itm = cfgSlot:item()
+  if itm then player.giveItem(itm) end
+end
+
+--function cfgSlot:acceptsItem(itm) return (itm or {count=0}).count == 0 end
+function cfgSlot:onItemModified()
+  pane.dismiss()
 end

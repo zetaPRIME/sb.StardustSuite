c7af61e3b9c2411cf073bde732c215ca5ac489a2
zetaPRIME <qmanxt@gmail.com>
2020-06-16 17:37:09 -0400

    recipe condition

--- StardustLib/sys/stardust/quickbar/conditions.lua
+++ StardustLib/sys/stardust/quickbar/conditions.lua
@@ -23,5 +23,6 @@ function conditions.statPositive(stat) return status.statPositive(stat) end
 function conditions.statNegative(stat) return not status.statPositive(stat) end
 function conditions.species(species) return player.species() == species end
 function conditions.ownShip() return player.worldId() == player.ownShipWorldId() end
+function conditions.recipe(name) return player.isAdmin() or player.blueprintKnown { name = name, count = 1 } end
 
 function conditions.configExists(key) return root.assetJson(key) ~= nil end

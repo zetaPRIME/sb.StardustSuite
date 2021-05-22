5328664a611096f245a47d6dfda5c40a4a254805
zetaPRIME <qmanxt@gmail.com>
2021-05-21 20:18:02 -0400

    progress on smelting slot

--- StarTech/startech/ui/machines/autosmelter.lua
+++ StarTech/startech/ui/machines/autosmelter.lua
@@ -12,6 +12,12 @@ metagui.startEvent(function() -- sync
     local data = waitFor(world.sendEntityMessage(pane.sourceEntity(), "uiSyncRequest")):result()
     if data then
       burnSlot:setItem(data.smelting.item)
+      local bsb = burnSlot.subWidgets.slot
+      if data.smelting.item.count >= 1 then
+        widget.setItemSlotProgress(bsb, ( (data.smelting.remaining or 0) / (data.smelting.smeltTime or 1) ))
+      else
+        widget.setItemSlotProgress(bsb, 1)
+      end
       
       fpLabel:setText(string.format("%i^gray;/^reset;%i^gray;FP^reset;", math.floor(0.5 + (data.batteryStats.energy or 0)), math.floor(0.5 + (data.batteryStats.capacity or 0))))
     end

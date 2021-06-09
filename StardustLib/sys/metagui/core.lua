591132cac4713f4094bb9c0574d9dcb60b1a9ba5
zetaPRIME <qmanxt@gmail.com>
2021-06-09 10:09:58 -0400

    metaGUI: don't need that

--- StardustLib/sys/metagui/core.lua
+++ StardustLib/sys/metagui/core.lua
@@ -425,7 +425,7 @@ function init() ----------------------------------------------------------------
     local size = mg.cfg.totalSize
     wheel.proto = {
       type = "scrollArea", position = {0, 0}, size = size, verticalScroll = false, children = {
-        fill = { type = "widget", position = {0, 0}, size = size },
+        --fill = { type = "widget", position = {0, 0}, size = size },
         target = { type = "widget", position = wheel.offset, size = {size[1], 72} },
         over = { type = "widget", position = wheel.offset, size = {size[1], 1000} },
       }

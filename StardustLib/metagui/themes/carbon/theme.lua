9e2e717a32875941447886846d9a5feee53819b4
Zithia Satazaki <qmanxt@gmail.com>
2023-11-01 19:53:36 -0400

    convert Carbon theme to dynamic layout

--- StardustLib/metagui/themes/carbon/theme.lua
+++ StardustLib/metagui/themes/carbon/theme.lua
@@ -7,31 +7,43 @@ local assets = theme.assets
 theme.scrollBarDirectives = "?brightness=50?multiply=" .. mg.getColor("accent")
 
 local titleBar, icon, title, close
+local fw = { }
 function theme.decorate()
-  local style = mg.cfg.style
-  widget.addChild(frame.backingWidget, { type = "canvas", position = {0, 0}, size = frame.size }, "canvas")
+  mg.widgetContext = fw
+  frame.mode = "vertical"
   
+  local style = mg.cfg.style
   if (style == "window") then
-    titleBar = frame:addChild { type = "layout", position = {3, 0}, size = {frame.size[1] - 7, 22}, mode = "horizontal" }
-    icon = titleBar:addChild { type = "image" }
-    title = titleBar:addChild { type = "label", expand = true, align = "left" }
-    close = titleBar:addChild{ type = "button", caption = "×", captionOffset = {0.5, -0.5}, color = "ff3f3f", size = {13, 13} }
-    function close:onClick()
+    frame:addChild { id = "bg", type = "layout", expandMode = {2, 2}, canvasBacked = true, mode = "vertical", spacing = 0, children = {
+      { { size = 22 }, 3,
+        { id = "icon", type = "image" }, 2,
+        { id = "title", type = "label", expand = true, align = "left" }, 2,
+        { id = "closeButton", type = "button", caption = "×", captionOffset = {0.5, -0.5}, color = "ff3f3f", size = {13, 13} },
+        4
+      }
+    } }
+    
+    function fw.closeButton:onClick()
       pane.dismiss()
     end
+  else
+    frame:addChild { id = "bg", type = "layout", expandMode = {2, 2}, canvasBacked = true }
   end
+  frame:updateGeometry() -- kick things to make ninepatch draw correctly the first time
+  
+  mg.widgetContext = nil
 end
 
 function theme.drawFrame()
-  local style = mg.cfg.style
-  c = widget.bindCanvas(frame.backingWidget .. ".canvas")
+  c = widget.bindCanvas(fw.bg.subWidgets.canvas)
   c:clear() assets.frame:draw(c)
   
+  local style = mg.cfg.style
   if (style == "window") then
     assets.button:draw(c, "accent?multiply=" .. mg.getColor("accent"), {0, frame.size[2] - 22, frame.size[1], frame.size[2]})
-    icon.explicitSize = (not mg.cfg.icon) and {0, 0} or nil
-    icon:setFile(mg.cfg.icon)
-    title:setText(mg.cfg.title)
+    fw.icon.explicitSize = (not mg.cfg.icon) and {0, 0} or nil
+    fw.icon:setFile(mg.cfg.icon)
+    fw.title:setText(mg.cfg.title)
   end
 end
 

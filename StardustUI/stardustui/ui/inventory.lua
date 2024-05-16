6ec2fd87e1ac154d49e13a6e0cdc1b8054b81302
Zithia Satazaki <qmanxt@gmail.com>
2024-05-15 23:54:58 -0400

    draw the rest of the fucking owl

--- StardustUI/stardustui/ui/inventory.lua
+++ StardustUI/stardustui/ui/inventory.lua
@@ -2,6 +2,9 @@
 
 -- /run player.interact("ScriptPane", { gui = { }, scripts = {"/metagui.lua"}, config = "stardustui:inventory" })
 
+require "/scripts/util.lua"
+require "/lib/stardust/itemutil.lua"
+
 local mg = metagui
 local ipc = getmetatable''["stardustui:"]
 if not ipc then ipc = { } getmetatable''["stardustui:"] = ipc end
@@ -47,6 +50,7 @@ itemGrid:subscribeEvent("tabChanged", function(self, tab)
   end
 end)
 
+itemGrid.onCaptureMouseMove = mg.widgetTypes.button.onCaptureMouseMove
 function itemGrid:onSlotMouseEvent(btn, down) -- remember, self is the *slot*, not the grid
   if down then self:captureMouse(btn) return
   elseif btn == self:mouseCaptureButton() then
@@ -56,19 +60,58 @@ function itemGrid:onSlotMouseEvent(btn, down) -- remember, self is the *slot*, n
   local bag = bagTabs.currentTab._bag
   if not bag then return end
   local sd = {bag.name, self.index - 1}
+  local itm = player.item(sd)
   
   local shift = mg.checkShift()
-  if shift then
-    if btn ~= 0 then return end -- only left button
-    -- only into an open container
+  if shift and btn == 0 then -- shift+lclick into an open container
     if not ipc.openContainerId or not world.containerSize(ipc.openContainerId) then return end
-    local itm = player.item(sd)
     if not itm then return end -- no item
     local lo = world.containerItemsFitWhere(ipc.openContainerId, itm).leftover
     local res = world.containerAddItems(ipc.openContainerId, itm)
     res.count = lo
     player.setItem(sd, res)
     self:setItem(res) -- update immediately
+  else -- normal click
+    local stm = player.swapSlotItem()
+    if not itm and not stm then return end -- nothing to do
+    --if itm and stm and not root.itemDescriptorsMatch(itm, stm) then return end -- 
+    if stm and not player.itemAllowedInBag(bag.name, stm) then return end -- carrying forbidden item; game will reject placement
+    
+    if btn == 0 then -- left click
+      if not mg.itemsCanStack(itm, stm) then -- heterogenous: plonk in/swap
+        player.setItem(sd, stm)
+        player.setSwapSlotItem(itm)
+      else -- homogenous: attempt to stack into
+        -- we know both items exist and are the same
+        local maxStack = mg.itemMaxStack(itm)
+        local xf = math.min(maxStack - itm.count, stm.count) -- max transfer
+        if xf > 0 then -- we want to transfer more than zero, do the thing
+          itm.count = itm.count + xf
+          stm.count = stm.count - xf
+          player.setItem(sd, itm)
+          player.setSwapSlotItem(stm)
+        end
+      end
+    elseif btn == 2 and (not stm or mg.itemsCanStack(itm, stm)) then
+      -- homogenous or none only: attempt to pull one/half into swap slot
+      if not stm then -- zero stack
+        stm = util.mergeTable({ }, itm)
+        stm.count = 0
+      end
+      local maxStack = mg.itemMaxStack(itm)
+      local xf = math.min(maxStack - stm.count, itm.count) -- max transfer
+      if shift then
+        xf = math.min(xf, math.max(math.floor(itm.count/2), 1))
+      else
+        xf = math.min(xf, 1)
+      end
+      if xf > 0 then -- we want to transfer more than zero, do the thing
+        itm.count = itm.count - xf
+        stm.count = stm.count + xf
+        player.setItem(sd, itm)
+        player.setSwapSlotItem(stm)
+      end
+    end
   end
 end
 
